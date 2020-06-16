8bed469291665f455419c5ab93f6a2c32e0fc595
chenweibo <563960993@qq.com>
2020-06-16 17:30:48 +0800

    first init

---
+++ src/main/java/com/chen/stencil/StencilApplication.java
@@ -0,0 +1,13 @@
+package com.chen.stencil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StencilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StencilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chen/stencil/StencilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.stencil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StencilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
