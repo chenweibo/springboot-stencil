e9e18e528db84978db8259ab44dc89153f4c4316
chenweibo <563960993@qq.com>
2020-06-17 11:15:22 +0800

    testRedis

--- src/test/java/com/chen/stencil/StencilApplicationTests.java
+++ src/test/java/com/chen/stencil/StencilApplicationTests.java
@@ -1,13 +1,26 @@
 package com.chen.stencil;
 
+import com.chen.stencil.utils.RedisUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StencilApplicationTests {
 
+    @Autowired
+    RedisUtils redisUtils;
+
     @Test
     void contextLoads() {
     }
+
+    @Test
+    public void testSetRedis() {
+        boolean status = redisUtils.set("testData", "EXMAPLE");
+        String c = redisUtils.get("testData");
+        System.out.println(c);
+    }
+
 
 }
