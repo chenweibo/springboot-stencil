412a8b5069200cfe77d9e9369af4177a13ccd32b
chenweibo <563960993@qq.com>
2020-06-17 10:05:48 +0800

    mybatis-plus CodeGenerator

--- src/main/java/com/chen/stencil/StencilApplication.java
+++ src/main/java/com/chen/stencil/StencilApplication.java
@@ -1,9 +1,11 @@
 package com.chen.stencil;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.chen.stencil.mapper")
 public class StencilApplication {
 
     public static void main(String[] args) {
